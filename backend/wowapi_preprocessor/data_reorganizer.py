b8063bef6ab64c4b097caf909310aca13e1e1cb5
oscheller1 <olescheller87@gmail.com>
2019-01-24 22:58:36 +0100

    Fix running of data_reorganizer in command line

--- backend/wowapi_preprocessor/data_reorganizer.py
+++ backend/wowapi_preprocessor/data_reorganizer.py
@@ -10,18 +10,78 @@ The following entities are put into the database:
 """
 import math
 from dataclasses import dataclass, asdict
-from typing import List
-
 from pymongo import MongoClient, ASCENDING
 import pandas as pd
 
-from backend.wowapi_preprocessor import get_item_classes, get_raw_items, get_auctions
+import os
+from typing import List
+
+wowdata_dir = os.path.abspath("../wowdata")
+wowdata_items_path = os.path.join(wowdata_dir, "items.json")
+wowdata_auction_house_path = os.path.join(wowdata_dir, "ah.json")
+wowdata_item_classes_path = os.path.join(wowdata_dir, "itemClasses.json")
+
+
+def get_auctions() -> List:
+    """Returns a list of auctions read from the local json file."""
+    with open(wowdata_auction_house_path, 'r') as f:
+        return json.load(f).get("auctions")
+
+
+def get_raw_items() -> List:
+    """Returns a list of items read from the local json file."""
+    with open(wowdata_items_path, 'r') as f:
+        return json.load(f).get("items")
+
+
+def get_item_classes() -> List:
+    """Returns a list of item classes read from the local json file."""
+    with open(wowdata_item_classes_path, 'r') as f:
+        return json.load(f).get("classes")
+
+
+def get_valid_item_classes(item_classes) -> List:
+    """Returns a list of valid item class ids."""
+    return [cls.get("class") for cls in item_classes]
+
 
 ITEM_ID = "item_id"
 SELLER = "seller"
 SELLER_REALM = "seller_realm"
 PRICE = "price"
 QUANTITY = "quantity"
+
+import json
+import os
+from typing import List
+
+wowdata_dir = os.path.abspath("../wowdata")
+wowdata_items_path = os.path.join(wowdata_dir, "items.json")
+wowdata_auction_house_path = os.path.join(wowdata_dir, "ah.json")
+wowdata_item_classes_path = os.path.join(wowdata_dir, "itemClasses.json")
+
+
+def get_auctions() -> List:
+    """Returns a list of auctions read from the local json file."""
+    with open(wowdata_auction_house_path, 'r') as f:
+        return json.load(f).get("auctions")
+
+
+def get_raw_items() -> List:
+    """Returns a list of items read from the local json file."""
+    with open(wowdata_items_path, 'r') as f:
+        return json.load(f).get("items")
+
+
+def get_item_classes() -> List:
+    """Returns a list of item classes read from the local json file."""
+    with open(wowdata_item_classes_path, 'r') as f:
+        return json.load(f).get("classes")
+
+
+def get_valid_item_classes(item_classes) -> List:
+    """Returns a list of valid item class ids."""
+    return [cls.get("class") for cls in item_classes]
 
 
 @dataclass
@@ -62,6 +122,7 @@ class SellOrder(object):
     seller_realm: str
     price: int
     quantity: int
+
 
 def raw_api_auctions_to_sell_orders(raw_api_auctions: list) -> List[SellOrder]:
     """Converts a list of raw api auctions to a list of final Sell Orders.
