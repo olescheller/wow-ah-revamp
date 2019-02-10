f9145cc054dbd66c838b9f6d8867873746edd4dd
oscheller1 <olescheller87@gmail.com>
2019-02-10 15:12:47 +0100

    Add apollo client

--- frontend/react/src/index.js
+++ frontend/react/src/index.js
@@ -12,16 +12,26 @@ import rootSaga from './redux/sagas'
 import theme from './theme/materialTheme'
 import reducer from './redux/reducer'
 
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+    uri: "https://localhost:4000"
+});
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk, sagaMiddleware)));
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
+    <ApolloProvider client={client}>
+
     <Provider store={store}>
         <MuiThemeProvider theme={theme}>
             <BrowserRouter>
                 <App/>
             </BrowserRouter>
         </MuiThemeProvider>
-    </Provider>, document.getElementById('root'));
+    </Provider></ApolloProvider>, document.getElementById('root'));
