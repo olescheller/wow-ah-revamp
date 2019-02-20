243ba3276c5e309ebfeb1c91c28957c54912b75b
Ole Scheller <olescheller87@gmail.com>
2019-02-20 22:39:55 +0100

    Implement unittests

--- frontend/react/test/Actions.test.js
+++ frontend/react/test/Actions.test.js
@@ -2,7 +2,7 @@ import * as actions from '../src/redux/actions/actions'
 import * as itemActions from '../src/redux/actions/itemActions'
 
 describe('actions', () => {
-    it('should create an action to add a exceed quantity of an item', () => {
+    it('should create an action to notify of exceeded quantity of an item', () => {
         const expectedAction = {
             type: actions.QUANTITY_EXCEEDED,
             payload: {itemId: 1337}
@@ -10,8 +10,9 @@ describe('actions', () => {
         expect(actions.quantityExceededAction(1337)).toEqual(expectedAction)
     })
 });
+
 describe('actions', () => {
-    it('should create an action to add a exceed quantity of an item', () => {
+    it('should create an action to set loading', () => {
         const expectedAction = {
             type: actions.SET_LOADING,
             payload: true
