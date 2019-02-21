21764e4d6e51d8d140da77bbc75ede21789a4c52
Ole Scheller <olescheller87@gmail.com>
2019-02-21 20:11:39 +0100

    Update ec2 ip

--- frontend/react/src/config/config.js
+++ frontend/react/src/config/config.js
@@ -1,3 +1,3 @@
 export const inventorySize = 24;
 
-export const ec2_url =  "ec2-18-184-5-210.eu-central-1.compute.amazonaws.com";
+export const ec2_url =  "ec2-52-29-134-149.eu-central-1.compute.amazonaws.com";
