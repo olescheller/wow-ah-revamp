2251f3db1d0ebe72cfaef65910d34ecd0819d41a
Ole Scheller <olescheller87@gmail.com>
2019-03-08 16:14:14 +0100

    Implement list for selling items

--- frontend/elm/src/Page/Sell.elm
+++ frontend/elm/src/Page/Sell.elm
@@ -2,37 +2,52 @@ module Page.Sell exposing (..)
 
 
 import Action exposing (Msg(..))
-import Html exposing (Html, br, div, img, span, text)
+import Html exposing (Html, a, br, div, h1, img, span, text)
 import Html.Attributes exposing (class, src)
 import Html.Events exposing (onClick)
 import Lib.StringHelper exposing (getItemIconUrl)
 import Maybe exposing (withDefault)
-import State exposing (InventorySlot, Item, State)
+import State exposing (InventorySlot, Item, ItemSupply, State)
 
 
 displayInventory : State -> Html Msg
 displayInventory model =
-    div [class "ui grid"]
+    div[][
+    h1[] [text "Sell items"]
+    , div [class "ui grid"]
     [
-    div [](List.map displayInventorySlot model.data.userInventory)
+    div [class "ui items "](List.map displayInventorySlot model.data.userInventory)
     ,div [][text model.detailItem.item.name]
     ]
-
+    ]
 displayInventorySlot : Maybe InventorySlot -> Html Msg
 displayInventorySlot inventorySlot =
     case inventorySlot of
         Just value ->
-            div[][displayItem value.item value.quantity]
+            displayItem value.item value.quantity
         Nothing ->
             div[][]
 
 
 displayItem : Item -> Int -> Html Msg
 displayItem item quantity =
-    div[onClick (RequestItemDetail item.name) ][
-        img[ src <| getItemIconUrl item ][]
-        , span[][text item.name]
-        , span[][text (String.fromInt quantity)]
+    div[ class "item", onClick (RequestItemDetail item.name) ][
+        div[ class "ui tiny image circular" ][ img[ src <| getItemIconUrl item ][] ]
+        , div[ class "content"][
+            a [ class "header"][text item.name]
+            , div [class "description"][
+                span[][text ("Owned quantity: " ++ (String.fromInt quantity))]
+            ]
+        ]
+
+    ]
+
+displayItemDetail : ItemSupply -> Html Msg
+displayItemDetail itemSupply =
+    div [][
+        div [][
+        ]
+        , div [][]
     ]
 
 
