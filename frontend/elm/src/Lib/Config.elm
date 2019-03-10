a3531fbbe80525a68dfb27f5778276d81881f340
oscheller1 <olescheller87@gmail.com>
2019-03-10 18:26:19 +0100

    Update elm config

--- frontend/elm/src/Lib/Config.elm
+++ frontend/elm/src/Lib/Config.elm
@@ -2,4 +2,4 @@ module Lib.Config exposing (..)
 
 graphQlServerUrl : String
 graphQlServerUrl =
-    "http://ec2-18-184-211-37.eu-central-1.compute.amazonaws.com:8080/"
+    "http://ec2-18-184-211-37.eu-central-1.compute.amazonaws.com:4000/"
