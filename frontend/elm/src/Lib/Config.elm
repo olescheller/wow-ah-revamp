72ed607ef3784002edfb7b7672b0bb51a40f49c5
oscheller1 <olescheller87@gmail.com>
2019-03-10 18:59:04 +0100

    Update ec2 instance config

--- frontend/elm/src/Lib/Config.elm
+++ frontend/elm/src/Lib/Config.elm
@@ -2,4 +2,4 @@ module Lib.Config exposing (..)
 
 graphQlServerUrl : String
 graphQlServerUrl =
-    "http://ec2-18-184-211-37.eu-central-1.compute.amazonaws.com:4000/"
+    "http://ec2-3-121-199-151.eu-central-1.compute.amazonaws.com:4000/"
--- frontend/react/src/config/config.js
+++ frontend/react/src/config/config.js
@@ -1,3 +1,3 @@
 export const inventorySize = 24;
 
-export const ec2_url =  "ec2-18-184-211-37.eu-central-1.compute.amazonaws.com";
+export const ec2_url =  "ec2-3-121-199-151.eu-central-1.compute.amazonaws.com";
