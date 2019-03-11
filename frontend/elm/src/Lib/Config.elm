29014deb5dbd26893be63d5fabf368ea35f16baa
J.Martensen <jule.m.martensen@student.fh-kiel.de>
2019-03-11 10:21:54 +0100

    Change config url to deployed backend

--- frontend/elm/src/Lib/Config.elm
+++ frontend/elm/src/Lib/Config.elm
@@ -1,6 +1,6 @@
 module Lib.Config exposing (..)
 
 graphQlServerUrl : String
---graphQlServerUrl =
-   -- "http://ec2-3-121-199-151.eu-central-1.compute.amazonaws.com:4000/"
-graphQlServerUrl = "http://localhost:4000/"
+graphQlServerUrl =
+    "http://ec2-3-121-199-151.eu-central-1.compute.amazonaws.com:4000/"
+--graphQlServerUrl = "http://localhost:4000/"
